5e13e3f6885b0d726ac8673ad4ad0f7392c96e73
Okorie Daberechi <90217696+Dahbaeraychy@users.noreply.github.com>
2025-04-12 09:04:04 +0000

    Fix Build issues
    Removed all unncessary files
    Fixed image download in firefox and safari with skip fonts
    
    Bug: T388248

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,54 +8,54 @@ import { toPng } from "html-to-image";
 import Footer from "@/components/shared/Footer/Footer";
 
 const initialData: FormData = {
-  title: "Young Black Girl From Africa",
-  description:
-    "Curiosity started it, creativity fuels it. From 17-year-old app builder to product thinker, I build meaningful, accessible experiences that spark connection, insight, and the occasional “aha.”",
-  image: "/hero-image.png",
-  bgColor: "#222222",
+	title: "Young Black Girl From Africa",
+	description:
+		"Curiosity started it, creativity fuels it. From 17-year-old app builder to product thinker, I build meaningful, accessible experiences that spark connection, insight, and the occasional “aha.”",
+	image: "/hero-image.png",
+	bgColor: "#222222",
 };
 
 export default function BannerPage() {
-  const [formData, setFormData] = useState<FormData>(initialData);
-  const [selectedBannerStyle, setSelectedBannerStyle] = useState("modern");
+	const [formData, setFormData] = useState<FormData>(initialData);
+	const [selectedBannerStyle, setSelectedBannerStyle] = useState("modern");
 
-  const componentRef = useRef<HTMLDivElement>(null);
+	const componentRef = useRef<HTMLDivElement>(null);
 
-  const handleDownload = async () => {
-    if (!componentRef.current) return;
+	const handleDownload = async () => {
+		if (!componentRef.current) return;
 
-    try {
-      const dataUrl = await toPng(componentRef.current);
-      const link = document.createElement("a");
-      link.download = "banner.png";
-      link.href = dataUrl;
-      link.click();
-    } catch (err) {
-      console.error("Error exporting image:", err);
-    }
-  };
+		try {
+			const dataUrl = await toPng(componentRef.current, { skipFonts: true });
+			const link = document.createElement("a");
+			link.download = "banner.png";
+			link.href = dataUrl;
+			link.click();
+		} catch (err) {
+			console.error("Error exporting image:", err);
+		}
+	};
 
-  return (
-    <main className="max-w-[1200px] mx-auto w-full space-y-10 font-sans sm:p-10 ">
-      <Banner
-        {...formData}
-        ref={componentRef}
-        bannerStyle={selectedBannerStyle}
-      />
-      <div className="grid lg:grid-cols-3 w-full bg-neutral-50 dark:bg-dark-150 p-6 lg:py-12 divide-x-2 divide-neutral-200 dark:divide-dark-200 rounded-md ">
-        <div className="col-span-2">
-          <CustomizeForm defaultValues={formData} onUpdate={setFormData} />
-        </div>
-        <BannerStyles
-          onClick={handleDownload}
-          bannerStyle={selectedBannerStyle}
-          onChangeBannerStyle={(style: string) => {
-            setSelectedBannerStyle(style);
-          }}
-        />
-      </div>
+	return (
+		<main className="max-w-[1200px] mx-auto w-full space-y-10 font-sans sm:p-10 ">
+			<Banner
+				{...formData}
+				ref={componentRef}
+				bannerStyle={selectedBannerStyle}
+			/>
+			<div className="grid lg:grid-cols-3 w-full bg-neutral-50 dark:bg-dark-150 p-6 lg:py-12 divide-x-2 divide-neutral-200 dark:divide-dark-200 rounded-md ">
+				<div className="col-span-2">
+					<CustomizeForm defaultValues={formData} onUpdate={setFormData} />
+				</div>
+				<BannerStyles
+					onClick={handleDownload}
+					bannerStyle={selectedBannerStyle}
+					onChangeBannerStyle={(style: string) => {
+						setSelectedBannerStyle(style);
+					}}
+				/>
+			</div>
 
-      <Footer />
-    </main>
-  );
+			<Footer />
+		</main>
+	);
 }
